3fbefd6461d1a077075cf656f204289fd831589c
KBarons <KBarons@gmail.com>
2019-07-02 20:24:52 +0300

    Pievienotas datubāzes metodes userExists, userPwdCorrect, registerNewUser

--- src/main/java/com/fITsummer/Database.java
+++ src/main/java/com/fITsummer/Database.java
@@ -3,16 +3,15 @@ package com.fITsummer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
-
-import javax.annotation.PostConstruct;
 import java.sql.*;
+
 
 @Component
 public class Database {
     public static Logger logger = LoggerFactory.getLogger(Database.class);
 
-    Statement statement;
-    String username;
+//    Statement statement;
+//    String username;
 
     protected Connection conn = null;
     private final String DB_URL = "jdbc:mysql://127.0.0.1:3306/?autoReconnect=true&useSSL=false&characterEncoding=utf8&serverTimezone=GMT ";
@@ -20,10 +19,31 @@ public class Database {
     private final String PASS = "Student007";
     private PreparedStatement preparedStatement = null;
 
-    public Database() {
-//        logger.info("test printing");
+//testa nolūkiem datubāzes izdrukāšana, kamēr taisam projektu
+    public static void main(String[] args) throws SQLException {
+        Database ourDB = new Database();
+        System.out.println(ourDB);
+/*        System.out.println(ourDB.userExists("renata"));
+        System.out.println(ourDB.userExists("dsfgdsfgsdf"));
+        System.out.println(ourDB.userPwdCorrect("renata", "parole1"));
+        System.out.println(ourDB.userPwdCorrect("renata", "pasdfgdsfsrole1"));
+        System.out.println(ourDB.userPwdCorrect("renasdfsdfta", "parole1"));
+        ourDB.registerNewUser("randomuser", "parole");
+        System.out.println(ourDB.userExists("randomuser"));*/
+    }
 
-        try {
+    public Database() {
+        logger.info("test printing");
+        try
+        {
+            try
+            {
+                Class.forName("com.mysql.cj.jdbc.Driver"); //loading mysql driver
+            }
+            catch (ClassNotFoundException e)
+            {
+                e.printStackTrace();
+            }
             conn = DriverManager.getConnection(DB_URL, USER, PASS);
             conn.setAutoCommit(false);
         } catch (
@@ -32,58 +52,37 @@ public class Database {
         }
     }
 
-    @PostConstruct
-    public void something() {
-        try {
-            userExists("renata");
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
 
     public boolean userExists(String username) throws SQLException {
-        Statement statement;
-        statement = conn.createStatement();
-        String q = "SELECT * FROM fITsummer.user_info WHERE username like '" + username+"'";
-        ResultSet rs = statement.executeQuery(q);
-        if (rs.next()) {
-            return true;
-        } else
-            return false;
-    }
+        preparedStatement = conn.prepareStatement("SELECT username FROM fITsummer.user_info WHERE username like ?");
+        preparedStatement.setString(1, username);
+        ResultSet rs = preparedStatement.executeQuery();
+            while (rs.next()) {
+            System.out.println("Username " + username + " already exists");
+                return true;
+            } return false;
+}
+
 
     //return true if combination username+password is correct
-    public boolean userPwdCorrect(String username, String password) {
-
-        return false;
+    public boolean userPwdCorrect(String username, String password) throws SQLException{
+        preparedStatement = conn.prepareStatement("SELECT username, password FROM fITsummer.user_info WHERE username like ? and password like ?");
+        preparedStatement.setString(1,username);
+        preparedStatement.setString(2, password);
+        ResultSet rs = preparedStatement.executeQuery();
+        while (rs.next()) {
+            System.out.println("Username " + username + " and password correct");
+            return true;
+        } return false;
     }
 
     //register new user with passed username and password.
-    public void registerNewUser(String username, String password) {
-
+    public void registerNewUser(String username, String password) throws SQLException{
+        preparedStatement = conn.prepareStatement("INSERT INTO fITsummer.user_info(username, password) VALUES (?, ?)");
+        preparedStatement.setString(1, username);
+        preparedStatement.setString(2, password);
+        int i = preparedStatement.executeUpdate();
+        conn.commit();
+        System.out.println(i + "users inserted");
     }
-
-    //set passed token in db for user with passed username
-    public void setRefreshToken(String username, String refreshToken) {
-
-    }
-
-    //set passed token in db for user with passed username
-    public void setAccessToken(String username, String accessToken) {
-
-    }
-
-    //get token for user with passed username
-    public String getRefreshToken(String username) {
-
-        return null;
-    }
-
-    //get token for user with passed username
-    public String getAccessToken(String username) {
-
-        return null;
-
-    }
-
-}
+}//end main
